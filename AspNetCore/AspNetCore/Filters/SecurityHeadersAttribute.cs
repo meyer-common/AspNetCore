730fca2590b5f7507ec07ed32a98293dde07c28e
Adam Meyer <cmeyerad@jci.com>
2019-08-31 18:29:19 -0500

    moved to github

---
+++ AspNetCore/AspNetCore/Filters/SecurityHeadersAttribute.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Meyer.Common.AspNetCore.Filters
+{
+    public class SecurityHeadersAttribute : ActionFilterAttribute
+    {
+        public bool CanCache { get; }
+
+        public SecurityHeadersAttribute(bool canCache)
+        {
+            this.CanCache = canCache;
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            IHeaderDictionary headers = context.HttpContext.Response.Headers;
+
+            if (!headers.ContainsKey("X-Content-Type-Options"))
+                headers.Add("X-Content-Type-Options", "nosniff");
+
+            if (!headers.ContainsKey("X-Frame-Options"))
+                headers.Add("X-Frame-Options", "SAMEORIGIN");
+
+            if (!headers.ContainsKey("Content-Security-Policy"))
+                headers.Add("Content-Security-Policy", "default-src 'self'; object-src 'none'; frame-ancestors 'none'; sandbox allow-forms allow-same-origin allow-scripts; base-uri 'self';");
+
+            if (!headers.ContainsKey("X-Content-Security-Policy"))
+                headers.Add("X-Content-Security-Policy", "default-src 'self'; object-src 'none'; frame-ancestors 'none'; sandbox allow-forms allow-same-origin allow-scripts; base-uri 'self';");
+
+            if (!headers.ContainsKey("Content-Disposition"))
+                headers.Add("Content-Disposition", "inline");
+
+            if (!this.CanCache && !headers.ContainsKey("Cache-Control"))
+                headers.Add("Cache-Control", "no-store");
+        }
+    }
+}
---
+++ AspNetCore/AspNetCore/ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Meyer.Common.AspNetCore
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IMvcCoreBuilder AddMvcMin(this IServiceCollection services)
+        {
+            return services
+                .AddMvcCore()
+                .AddJsonFormatters(x =>
+                {
+                    x.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                    x.NullValueHandling = NullValueHandling.Ignore;
+                    x.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+                });
+        }
+
+        public static IServiceCollection AddApiVersioning(this IServiceCollection services, int defaultMajor = 1, int defaultMinor = 0)
+        {
+            return services.AddApiVersioning(options =>
+            {
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ApiVersionReader = new QueryStringApiVersionReader(parameterNames: "v");
+                options.DefaultApiVersion = new ApiVersion(defaultMajor, defaultMinor);
+                options.ReportApiVersions = true;
+            });
+        }
+    }
+}
